1b9db5c878e40b638b866c39717726fb0a57ee64
Jonathan.Vorce <Jonathan.Vorce@gmail.com>
2016-10-15 21:56:26 -0500

    new class: Ship
    Ship defines the various actions that a ship can perform.
    The field the ship exists in is a member object of the ship class, and
    is passed in during Ship object construction.

---
+++ Ship.py
@@ -0,0 +1,53 @@
+import pdb
+
+###########################################################    
+# definition of ship
+#
+# constructor requires:
+#       a defined field class object <Field>
+#       definitions for movements commands <dictionary>
+#       definitions for fire pattern commands <dictionary>
+#
+# Supplied commands should take the form of:
+#       # pattern name and a list of x/y offsets measured from ownship position
+#       fire_commands = {
+#           'fire_pattern1' : [(x1,y1),(x2,y2),(x2,y2), ...],
+#           'fire_pattern2' : [(x1,y1),(x2,y2),(x2,y2), ...],
+#            ...                                            }
+#
+#       # move name and a list of composite moves
+#       # valid values are: 'north', 'south', 'east', 'west', ''
+#       move_commands = {}
+#           'NW' : [ 'north', 'west' ],
+#           'NE' : [ 'north', 'east' ],
+#            ...                         }
+###########################################################
+class Ship:
+    def __init__(self,field_def,move_cmds={},fire_cmds={}):
+        self.field = field_def
+        self.fire_commands = {}
+        self.move_commands = {
+            'north' : ['north'],
+            'south' : ['south'],
+            'east'  : ['east' ],
+            'west'  : ['west' ],
+            ''      : [''     ]}  # Fall
+
+
+        self.fire_commands.update(move_cmds)
+        self.move_commands.update(fire_cmds)
+
+    # Wrapper for all command functions
+    def Command(self,command):
+        _cmd_list = []
+        if self.move_commands.get(command):
+            _cmd_list = self.move_commands[command]
+            _f = self.field.Move
+        elif self.fire_commands.get(command):
+            _cmd_list = self.fire_commands[command]
+            _f = self.field.Fire
+            
+        for i in _cmd_list:
+            _f(command)
+
+#end class Ship class
